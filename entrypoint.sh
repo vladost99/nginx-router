9400bbd24d3167be44cf3a7a34ac4042b45e3c04
Vlad <vladsa000@gmail.com>
2025-05-06 13:22:41 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # Replace environment variables in nginx.conf
 envsubst '${FRONTEND_CONTAINER} ${FRONTEND_PORT} ${FRONTEND_PAGE_CONTAINER} ${FRONTEND_PAGE_PORT} ${BACKEND_API_CONTAINER} ${BACKEND_API_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+# Для дебагу: показати, що ми дійсно замінили змінні
+echo "===== Nginx Config with Substituted Variables ====="
+cat /etc/nginx/nginx.conf
+echo "=================================================="
+
 echo "Nginx configuration file has been prepared with environment variables."
 
 exit 0 
